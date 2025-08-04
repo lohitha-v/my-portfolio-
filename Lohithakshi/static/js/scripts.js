b394dccb2c5e969d3d5ae7bde1e0a012f97fed86
lohitha-v <v.lohithakshi@gmail.com>
2025-08-04 07:58:05 +0530

    Delete Lohithakshi directory

--- Lohithakshi/static/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll for dropdown menu links
-        document.querySelectorAll('.dropdown-content a').forEach(link => {
-            link.addEventListener('click', function(e) {
-                e.preventDefault();
-                const targetId = this.getAttribute('href').slice(1);
-                document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-            });
-        });
